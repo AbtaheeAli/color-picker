37b6a21a6d1d094823e3a1112605ee753d61d20e
Abtahee Ali <abtaheeali@gmail.com>
2020-06-23 17:09:25 -0400

    Finished HTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
 
-class App extends Component {
+export class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <main>
+        <section>
+          <h1>Choose Your Color!</h1>
+        </section>
+        <section>
+          <article>
+            <h2>Hue</h2>
+            <input
+              type="range"
+              min="0"
+              max="360"
+              value="this.state.h"
+              onChange={this.handleChangeH}
+            ></input>
+          </article>
+          <article>
+            <h2>Saturation</h2>
+            <input
+              type="range"
+              min="0"
+              max="360"
+              value="this.state.s"
+              onChange={this.handleChangeS}
+            ></input>
+          </article>
+          <article>
+            <h2>Lightness</h2>
+            <input
+              type="range"
+              min="0"
+              max="360"
+              value="this.state.l"
+              onChange={this.handleChangeL}
+            ></input>
+          </article>
+        </section>
+      </main>
+    )
   }
 }
 
